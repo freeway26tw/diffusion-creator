a4ab6c28bae16c1561a1b4b274e46d7ec03cc12d
freeway26tw <freeway26tw@gmail.com>
2023-05-14 23:33:53 +0800

    feat: error handler

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ const diffusion = require('./modules/diffusion')
 const users = require('./modules/users')
 
 const userController = require('../controllers/user-controller')
+const { apiErrorHandler } = require('../middleware/error-handler')
 
 router.post('/users', userController.signUp)
 
 router.use('/users', users)
 router.use('/diffusion', diffusion)
+
+router.use('/', apiErrorHandler)
 
 module.exports = router
