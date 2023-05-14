8f31d0e8b0444624c91d6c667bf577659e30994b
freeway26tw <freeway26tw@gmail.com>
2023-05-14 22:10:49 +0800

    feat: set users route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const diffusion = require('./modules/diffusion')
+const users = require('./modules/users')
 
+router.use('/users', users)
 router.use('/diffusion', diffusion)
 
 module.exports = router
